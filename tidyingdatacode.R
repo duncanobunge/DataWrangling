f34d050f1ca36cdc9c3d3481d8a12c6d9b37730d
DUNCAN OBUNGE <dancun.ogindo3@gmail.com>
2019-01-16 10:27:38 +0300

    Initial commit

---
+++ tidyingdatacode.R
@@ -0,0 +1,25 @@
+#load tidyverse package after installing it
+library(tidyverse)
+
+#load dataset bookings
+bookings<- read.csv("C:/Users/Bildad/Documents/Project/tidyingData/bookings.csv", header=TRUE)
+View(bookings)
+#load dataset properties
+properties<- read.csv("C:/Users/Bildad/Documents/Project/tidyingData/properties.csv", header=TRUE)
+properties
+
+#dplyr package- fxns are verbs,1st arg is data frame
+#returns data frame. subsequent args references colunm
+#use select()
+
+rbk <- select(bookings,review_score)
+rbk
+
+filter(bookings, status =="stayed" & is.na(review_score))
+x<- filter(bookings, price_per_night < 80)
+x <- select(x, room_nights, review_score)
+x
+
+#magrittr - use of pipe
+x %>% head(
+  select(room_nights, review_score),2)
